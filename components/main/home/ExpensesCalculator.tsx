90c6a3922e0ce3e13d5e4d8470b6b7815b9bd12e
lods <carlobaron430@gmail.com>
2026-03-07 13:34:45 +0800

    feat: expenses calculator dropdown done and calculation ignores 0 values

--- components/main/home/ExpensesCalculator.tsx
+++ components/main/home/ExpensesCalculator.tsx
@@ -7,12 +7,22 @@ import {
   Minus
 } from "@hugeicons/core-free-icons";
 import { HugeiconsIcon } from "@hugeicons/react"
-import { useState } from "react";
+import { useState, useRef } from "react";
+import { 
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuRadioGroup,
+  DropdownMenuLabel,
+  DropdownMenuRadioItem,
+} from "@/components/ui/dropdown-menu";
 
+type ExpenseCategory = 'Food' | 'Transport' | 'Wants' | 'Others';
 type ExpensesType = {
   id: number;
   value: number;
-  category: 'Food' | 'Transport' | 'Wants' | 'Others';
+  category: ExpenseCategory;
 }
 
 export function ExpensesCalculator(){
@@ -39,10 +49,10 @@ export function ExpensesCalculator(){
                       const updated = expenses.filter(item => item.id !== expense.id)
                       setExpenses(updated);
                     }}
-                    onChange={(value) => {
+                    onChange={(value, category) => {
                       setExpenses(prev =>
                         prev.map(item =>
-                          item.id === expense.id ? { ...item, value } : item
+                          item.id === expense.id ? { id: item.id, value, category } : item
                         )
                       );
                     }}
@@ -61,7 +71,7 @@ export function ExpensesCalculator(){
           >
             Add
           </Button>
-          <Button onClick={() => console.log(expenses)}>
+          <Button onClick={() => console.log(expenses.filter(expense => expense.value > 0))}>
             Calculate
           </Button>
         </div>
@@ -72,7 +82,7 @@ export function ExpensesCalculator(){
 
 interface ExpensesItemProps{
   onRemove: () => void;
-  onChange: (value: number) => void;
+  onChange: (value: number, category: ExpenseCategory) => void;
 }
 
 function ExpensesItem(
@@ -81,6 +91,15 @@ function ExpensesItem(
     onChange
   }:ExpensesItemProps
 ){
+  const [category, setCategory] = useState<ExpenseCategory>('Food');
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  function valueChanges(newCategory?: ExpenseCategory){
+    if(!inputRef.current) return;
+    const value = inputRef.current.valueAsNumber;
+    onChange(value, newCategory || category);
+  }
+
   return(
     <span className='flex items-center gap-2'>
       <Button 
@@ -90,13 +109,38 @@ function ExpensesItem(
         <HugeiconsIcon icon={Minus} />
       </Button>
       <Input 
+      ref={inputRef}
       type='number'
-      onChange={(e) => onChange(e.target.valueAsNumber || 0)}
+      onChange={() => valueChanges()}
       />
-      <Button>
-        Food
-        <HugeiconsIcon icon={ArrowDown01Icon} />
-      </Button>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button>
+            {category} 
+            <HugeiconsIcon icon={ArrowDown01Icon} />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuGroup>
+            <DropdownMenuLabel>
+              Categories
+            </DropdownMenuLabel>
+            <DropdownMenuRadioGroup 
+            value={category}
+            onValueChange={(newCategory) => {
+              const category = newCategory as ExpenseCategory 
+              valueChanges(category);
+              setCategory(category)
+            }}
+            >
+              <DropdownMenuRadioItem value='Food'>Food</DropdownMenuRadioItem>            
+              <DropdownMenuRadioItem value='Transport'>Transport</DropdownMenuRadioItem>            
+              <DropdownMenuRadioItem value='Wants'>Wants</DropdownMenuRadioItem>            
+              <DropdownMenuRadioItem value='Others'>Others</DropdownMenuRadioItem>            
+            </DropdownMenuRadioGroup>
+          </DropdownMenuGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </span>
   );
 }
